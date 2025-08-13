51cd106b062963a37cd0fd052f1726e8b5d2a7cd
Lucas Freitas <lucas.rodrigues.developer97@gmail.com>
2025-08-13 18:58:39 -0300

    create task model and relationships

---
+++ app/Models/Task.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Task extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'tasks';
+    protected $fillable = ['title','description','status'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
